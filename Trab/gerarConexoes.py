aeba9ca8f5fe2a205ab0ed56d57fb89623c14bdc
Guilherme Gomes <guilhermeneto26@hotmail.com>
2023-11-14 09:12:59 -0300

    Update gerarConexoes.py

--- Trab/gerarConexoes.py
+++ Trab/gerarConexoes.py
@@ -5,11 +5,14 @@ def printUsers(): #Motra usuarios no arquivo "teste.txt"
         usuarios = pickle.load(f)
         conexoes = pickle.load(f)
         d3 = pickle.load(f)
+        d4 = pickle.load(f)
     with open("teste.txt","wt") as g:
         for user in usuarios:
             g.write(f'{user}:{usuarios[user]}\n')
         g.write("\n\n\n")
         for conex in conexoes:
             g.write(f'{conex}:{conexoes[conex]}\n')
-        f.write(f'{d3}')
+        g.write("\n\n\n")
+        g.write(f'{d3}')
+        g.write(f'{d4}')
 printUsers()
