10a4d38d5ceb81269043d204c523732f82468f19
Guilherme Gomes <guilhermeneto26@hotmail.com>
2023-11-14 09:00:35 -0300

    Add files via upload
    Add input file "backup.py", the algorithm "gerarConexoes.py" and the output file "teste.txt"

---
+++ Trab/gerarConexoes.py
@@ -0,0 +1,15 @@
+import pickle
+
+def printUsers(): #Motra usuarios no arquivo "teste.txt"
+    with open("backup.bin", "rb") as f:
+        usuarios = pickle.load(f)
+        conexoes = pickle.load(f)
+        d3 = pickle.load(f)
+    with open("teste.txt","wt") as g:
+        for user in usuarios:
+            g.write(f'{user}:{usuarios[user]}\n')
+        g.write("\n\n\n")
+        for conex in conexoes:
+            g.write(f'{conex}:{conexoes[conex]}\n')
+        f.write(f'{d3}')
+printUsers()
