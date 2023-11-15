39b123a8a47ea3df931661229a5b1e7d26d529e5
Guilherme Gomes <guilhermeneto26@hotmail.com>
2023-11-15 00:11:25 -0300

    Update gerarConexoes.py

--- Trab/gerarConexoes.py
+++ Trab/gerarConexoes.py
@@ -4,8 +4,8 @@ def printUsers(): #Motra usuarios no arquivo "teste.txt"
     with open("backup.bin", "rb") as f:
         usuarios = pickle.load(f)
         conexoes = pickle.load(f)
-        d3 = pickle.load(f)
-        d4 = pickle.load(f)
+        l = pickle.load(f)
+        cont = 0
     with open("teste.txt","wt") as g:
         for user in usuarios:
             g.write(f'{user}:{usuarios[user]}\n')
@@ -13,6 +13,48 @@ def printUsers(): #Motra usuarios no arquivo "teste.txt"
         for conex in conexoes:
             g.write(f'{conex}:{conexoes[conex]}\n')
         g.write("\n\n\n")
-        g.write(f'{d3}')
-        g.write(f'{d4}')
-printUsers()
+        g.write(f'{l}\n')
+        for i in l:
+            if i[0] == "u00000000":
+                cont += 1
+        g.write(f"len(l):{len(l)}\n")
+        g.write(f"{cont}")
+#printUsers()
+
+def verificaConexoes(d):
+    for user in d:
+        for i in d[user][0]:
+            if i not in d[user][1]:
+                return True
+    return False
+
+def geraConexoes():
+    with open("backup.bin", "rb") as f:
+        usuarios = pickle.load(f)
+        conexoes = pickle.load(f)
+        historico = pickle.load(f)
+
+    for likes in historico:
+        deuLike = likes[0]
+        recebeLike = likes[1]
+        if recebeLike not in conexoes[deuLike][0] and recebeLike not in conexoes[deuLike][0]:
+            conexoes[deuLike][0].append(recebeLike)
+            conexoes[recebeLike][1].append(deuLike)
+    
+    for user in conexoes:
+        gostou = conexoes[user][0]
+        gostaram = conexoes[user][1]
+        mutuos = conexoes[user][2]
+        for i in gostou:
+            if i in gostaram:
+                mutuos.append(i)
+                gostou.remove(i)
+                gostaram.remove(i)
+    print(verificaConexoes(conexoes))
+    with open("teste.txt","wt") as g:
+        g.write(f"{usuarios}")
+        g.write(f"{conexoes}")
+    with open("dados.bin","wb") as b:
+        pickle.dump(usuarios,b)
+        pickle.dump(conexoes,b)
+geraConexoes()
